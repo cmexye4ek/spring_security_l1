82b904b481d75160c124ae4520f8f0adcd06e07a
cmexye4ek <89582141+cmexye4ek@users.noreply.github.com>
2022-07-04 13:46:21 +0500

    Initial commit

---
+++ src/main/java/ru/gb/spring_security_l1/SpringSecurityL1Application.java
@@ -0,0 +1,13 @@
+package ru.gb.spring_security_l1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityL1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityL1Application.class, args);
+	}
+
+}
